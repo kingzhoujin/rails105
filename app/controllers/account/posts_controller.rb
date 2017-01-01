70c74968ecb668089b093d9bc3e9da7b89f1bbb1
zhoujin <jin100chow@hotmail.com>
2017-01-01 23:03:48 +0800

    implement posts edit and delete

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -3,4 +3,16 @@ class Account::PostsController < ApplicationController
   def index
     @posts = current_user.posts
   end
+
+  def edit
+    @post = post.find(params[:id])
+    @post.save
+   redirect_to post_params, notice: "Edit Success"
+
+ end
+
+ def destroy
+   @post = post.find(params[:id])
+   @post.destroy
+ end
 end
